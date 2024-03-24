4e5fb24ecfc8412d002465d25c3cb02aa013e935
Abdelrahman Elzakazeky <42267852+Azakazeky@users.noreply.github.com>
2024-03-24 09:14:19 +0200

    init Socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServerGateway } from './socket/server.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,ServerGateway],
 })
 export class AppModule {}
---
+++ src/socket/server.gateway.ts
@@ -0,0 +1,63 @@
+import { OnModuleInit } from "@nestjs/common";
+import { SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway(3000,
+    { transports: ['websocket'] }
+)
+export class ServerGateway implements OnModuleInit {
+
+    @WebSocketServer()
+    server: Server;
+
+    onModuleInit() {
+        this.server.on('connection', (socket) => {
+            console.log(socket.id, 'connected');
+            console.log(new Date());
+            socket.send('Welcome to the server!');
+            return socket.id;
+        }
+        );
+    }
+    @SubscribeMessage('connect')
+    handleConnect(client: any, ...args: any[]) {
+        console.log("Client connected: ", client.id);
+        console.log(new Date());
+    }
+
+    @SubscribeMessage('disconnect')
+    handleDisconnect(client: any, ...args: any[]) {
+        console.log("Client disconnected: ", client.id);
+        console.log(new Date());
+    }
+
+    @SubscribeMessage('joinRoom')
+    handleRoomJoin(client: Socket, room: string) {
+        client.join(room);
+        console.log("Client joined room: ", room);
+        console.log(new Date());
+        client.emit('joinedRoom', room);
+    }
+
+    @SubscribeMessage('leaveRoom')
+    handleRoomLeave(client: Socket, room: string) {
+        client.leave(room);
+        console.log("Client left room: ", room);
+        console.log(new Date());
+        client.emit('leftRoom', room);
+    }
+
+    @SubscribeMessage('onMessage')
+    handleListenMessage(client: Socket, message: { sender: string, room: string, message: string }) {
+        this.server.to(message.room).emit('chatToClient', message);
+        console.log('message from ' + client.id);
+    }
+    @SubscribeMessage('sendMessage')
+    handleSendMessage(client: Socket, message: { sender: string, room: string, message: string }) {
+        this.server.to(message.room).emit('chatToClient', message);
+        console.log('message from ' + client.id);
+    }
+
+
+
+}
